c3093642b74b66c8aac604cb3597adf2eb29f1c3
matarillo <kentaro.inomata@gmail.com>
2020-02-05 14:55:43 +0900

    episode08

--- WeightedInteger.fs
+++ WeightedInteger.fs
@@ -3,7 +3,9 @@ module WeightedInteger
 open Probability
 
 let private sdu = StandardDiscreteUniform.distribution
+let private bern = Bernoulli.distribution
 
+// Weighted integer distribution using rejection sampling method.
 let distribution weights =
     if List.exists (fun x -> x < 0) weights then System.ArgumentException() |> raise
     if not <| List.exists (fun x -> x > 0) weights then System.ArgumentException() |> raise
@@ -11,17 +13,16 @@ let distribution weights =
     | [_]         -> Singleton.distribution 0
     | [zero; one] -> Bernoulli.distribution zero one
     | _           ->
-        // inverse transform sampling
         let weights = Array.ofList weights
-        let cumulateBack acc (x : int) =
-            match acc with
-            | head::tail -> (x + head)::head::tail
-            | []         -> [x]
-        let cumulative = weights |> Array.fold cumulateBack [] |> List.rev
+        let max = Array.max weights
+        let distributions = weights |> Array.map (fun w -> bern w (max - w))
         let fSample () =
-            let total = List.last cumulative
-            let uniform = sdu 1 total |> sample
-            List.findIndex (fun c -> c >= uniform) cumulative
+            let rows = sdu 0 (weights.Length - 1)
+            let rec loop () =
+                let row = rows |> sample
+                let s = distributions.[row] |> sample
+                if s = 0 then row else loop ()
+            loop ()
         let fSupport () = [0 .. weights.Length - 1] |> List.filter (fun i -> weights.[i] > 0)
         let fWeight i = if 0 <= i && i < weights.Length then weights.[i] else 0
         DiscreteDistribution<_>(fSample, fSupport, fWeight)
